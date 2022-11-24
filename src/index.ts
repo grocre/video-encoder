93de7f05955e6a874b3746f6af8d49fd582c5257
Gabriel Renato <grocre@gmail.com>
2022-11-24 15:45:03 -0300

    some changes on code structure

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import express, { Request, Response } from "express"
 
 const app = express()
 
+// vale a pena ver a aplicabilidade disso com websockets
+
 app.get("/serveApi", (request: Request, response: Response) => {
     response.json({"message": "okay"})
 })
 
 app.post("/requestInfo", (request: Request, response: Response) => {
     let {searchedData} = request.body
-    response.json({"message": "okay"})
 })
 
 app.listen(5000, () => console.log("server is running"))
+
